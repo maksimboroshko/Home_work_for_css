a31f3f79e4414a4f6887d04f3360afdadbb40898
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-10-12 21:21:20 +0300

    added fixes

--- src/test/java/TestForCssHw.java
+++ src/test/java/TestForCssHw.java
@@ -1,13 +1,8 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import com.codeborne.selenide.ElementsCollection;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-
 import java.io.File;
-
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestForCssHw {
@@ -31,6 +26,8 @@ public class TestForCssHw {
         $("#lastName").setValue("Testov");
         $("#userEmail").setValue("test@gmail.com");
         $("#userNumber").setValue("77777777777");
+        $("#dateOfBirthInput").sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), "1");
+        $("#dateOfBirthInput").setValue("8 Feb 2000").pressEnter();
         $("#currentAddress").setValue("testovoeSms");
         $("label[for='gender-radio-1']").click();
         $("label[for='hobbies-checkbox-1']").scrollTo().click();
@@ -39,13 +36,9 @@ public class TestForCssHw {
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Noida").pressEnter();
         $("#subjectsInput").setValue("English").pressEnter();
-        $("#dateOfBirthInput").sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), "18 Feb 2000");
-        $("#submit").click();
 
-        // думал что кнопка сабмита перекрыта чем-то . Что мешает , оказалось что поле #subjectsInput как-то голову делает
-        //JavascriptExecutor js = (JavascriptExecutor) Selenide.webdriver().driver().getWebDriver();
-        //js.executeScript("document.getElementById('RightSide_Advertisement').style.display='none';");
-        //js.executeScript("document.getElementById('userForm').style.display='none';");
+        $("#submit").scrollTo().doubleClick();
+
 
 
 
