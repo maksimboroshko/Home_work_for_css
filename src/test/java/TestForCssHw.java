c8b97dba5dc1a67a9b06831caaee986bb6205f3b
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-10-12 21:06:32 +0300

    added submit + english

--- src/test/java/TestForCssHw.java
+++ src/test/java/TestForCssHw.java
@@ -1,7 +1,9 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.ElementsCollection;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 
 import java.io.File;
@@ -17,6 +19,7 @@ public class TestForCssHw {
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
+
     }
 
     @Test
@@ -29,14 +32,22 @@ public class TestForCssHw {
         $("#userEmail").setValue("test@gmail.com");
         $("#userNumber").setValue("77777777777");
         $("#currentAddress").setValue("testovoeSms");
-        $("#dateOfBirthInput").sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), "18 Feb 2000");
         $("label[for='gender-radio-1']").click();
         $("label[for='hobbies-checkbox-1']").scrollTo().click();
         $("label[for='hobbies-checkbox-2']").scrollTo().click();
         $("label[for='hobbies-checkbox-3']").scrollTo().click();
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Noida").pressEnter();
-        $("#subjectsInput").setValue("testovoeSms"); // оставилп последним , т.к не понял как работать после заполнения этого поля
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#dateOfBirthInput").sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), "18 Feb 2000");
+        $("#submit").click();
+
+        // думал что кнопка сабмита перекрыта чем-то . Что мешает , оказалось что поле #subjectsInput как-то голову делает
+        //JavascriptExecutor js = (JavascriptExecutor) Selenide.webdriver().driver().getWebDriver();
+        //js.executeScript("document.getElementById('RightSide_Advertisement').style.display='none';");
+        //js.executeScript("document.getElementById('userForm').style.display='none';");
+
+
 
 
 
