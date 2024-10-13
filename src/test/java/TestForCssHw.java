4ee997cf3b48c123df37a7b1d09a808bbcc501be
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-10-13 16:03:15 +0300

    added holdBrowserOpen = false;

--- src/test/java/TestForCssHw.java
+++ src/test/java/TestForCssHw.java
@@ -14,7 +14,7 @@ public class TestForCssHw {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920 x 1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.timeout = 5000;
 
     }
