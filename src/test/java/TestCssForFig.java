70e6aea0f5139e47576eeaf4b8d3dcc276b30abd
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-10-16 19:04:13 +0300

    added fix steps

--- src/test/java/TestCssForFig.java
+++ src/test/java/TestCssForFig.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestCssForFig {
@@ -18,28 +19,27 @@ public class TestCssForFig {
     }
 
     public static String value1() {
-        return  "@ExtendWith({SoftAssertsExtension.class})\n" +
-                "class Tests {\n" +
-                "  @Test\n" +
-                "  void test() {\n" +
-                "    Configuration.assertionMode = SOFT;\n" +
-                "    open(&quot;page.html&quot;);\n" +
-                "\n" +
-                "    $(&quot;#first&quot;).should(visible).click();\n" +
-                "    $(&quot;#second&quot;).should(visible).click();\n" +
-                "  }\n" +
-                "}";
+        return   """
+        @ExtendWith({SoftAssertsExtension.class})
+        class Tests {
+          @Test
+          void test() {
+            Configuration.assertionMode = SOFT;
+            open("page.html");
 
+            $("#first").should(visible).click();
+            $("#second").should(visible).click();
+          }
+        }
+        """;
     }
-
     @Test
     void findTest() {
         open("selenide/selenide");
         $("#wiki-tab").click();
         $("#wiki-pages-box").$("button.Link--muted").scrollTo().click();
-        $(".wiki-rightbar").shouldHave(text("SoftAssertions")).click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(Condition.text(value1()));
 
-        $(".markdown-body").shouldHave(Condition.attribute("value", value1()));
-        //$(".markdown-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})"));
     }
 }
