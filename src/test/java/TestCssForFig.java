ccb78bcdd6dbb56989c47d5ca552097fa0f9b265
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-10-17 15:25:56 +0300

    delete import

--- src/test/java/TestCssForFig.java
+++ src/test/java/TestCssForFig.java
@@ -2,8 +2,6 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
