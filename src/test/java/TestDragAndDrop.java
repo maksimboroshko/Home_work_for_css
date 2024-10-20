2e78f07d507ab5f8e090fcf1006d62de0296a7e7
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-10-20 14:23:32 +0300

    hover + draganddrog

---
+++ src/test/java/TestDragAndDrop.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestDragAndDrop{
+
+    @BeforeAll
+    static void preconditionsForAllTests() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("/drag_and_drop");
+        //actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-b").dragAndDrop(DragAndDropOptions.to($("#column-a")));
+        $("#column-b").shouldHave(text("a"));
+    }
+}
